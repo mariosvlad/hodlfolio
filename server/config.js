5a12d83ff739929da8e7f886ef4af6adb5d27bbd
Marios Vladimirou <marios.vladimirou@gmail.com>
2018-04-07 03:20:35 +0300

    update dependencies

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-const defaultPort = process.env.NODE_ENV === 'test' ? 5001 : 5002;
+const defaultPort = process.env.NODE_ENV === 'test' ? 5001 : 5000;
 module.exports.port = process.env.PORT || defaultPort;
 module.exports.dbUrl =
   process.env.DATABASE_URL || 'postgres://postgres:postgres@127.0.0.1:5432/hodlfolio';
