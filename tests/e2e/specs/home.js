8f82a8bd0eabda0808f0a7052d73155202194037
Marios Vladimirou <marios.vladimirou@gmail.com>
2021-01-16 10:23:20 +0000

    chore: upgrade cypress, fix tests

--- tests/e2e/specs/home.js
+++ tests/e2e/specs/home.js
@@ -21,11 +21,12 @@ describe('Homepage', () => {
 
   it('should add 2 cryptocurrencies', () => {
     cy.get('.v-select.v-autocomplete input')
+      .first()
       .as('select')
       .click()
       .type('Bitcoin');
 
-    cy.get('.v-list-item__title')
+    cy.get('.v-select-list .v-list-item__title')
       .first()
       .click();
 
